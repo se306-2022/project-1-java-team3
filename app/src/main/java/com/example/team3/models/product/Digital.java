a6469503da11a036108cb5374219f0e3956c4f49
Matthew Ouyang <mouyang2001@gmail.com>
2022-08-21 13:50:30 +1200

    add product field to digital class

--- app/src/main/java/com/example/team3/models/product/Digital.java
+++ app/src/main/java/com/example/team3/models/product/Digital.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public class Digital extends Product {
     public Digital(int id, String name, String artist, int year, List<String> images, int price,
-                   String mainColour, String theme, String description, int viewCount) {
+                   String mainColour, String theme, String description, int viewCount, String category) {
 
-        super(id, name, artist, year, images, price, mainColour, theme, description, viewCount);
+        super(id, name, artist, year, images, price, mainColour, theme, description, viewCount, category);
     }
 
     public Digital() {}
